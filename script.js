6e5ad9dc326cecc52cdfee4be17ef661ac5b533a
souzza <cauelima752@gmail.com>
2025-10-29 23:27:35 -0300

    first commit

--- script.js
+++ script.js
@@ -199,3 +199,13 @@ backToTop.addEventListener('click', () => {
         lucide.createIcons();
     }, 500);
 });
+
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
